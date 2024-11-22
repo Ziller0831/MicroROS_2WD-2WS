a27a8a24f65dfa168977361b5e5c45371824cf35
Ziller0831 <wesley222666@gmail.com>
2024-11-22 13:41:11 +0800

    <feat>以初步撰寫好RobotBase_test程式，但還沒測試編碼器的計算功能

---
+++ RobotBase_test/lib/BaseController/motor_interface.h
@@ -0,0 +1,33 @@
+#ifndef __MOTOR_INTERFACE_H__
+#define __MOTOR_INTERFACE_H__
+
+class MotorInterface
+{
+    bool invert_;
+
+protected:
+    virtual void forward(int pwm) = 0;
+    virtual void reverse(int pwm) = 0;
+
+public:
+    MotorInterface(int invert) : invert_(invert)
+    {
+    }
+
+    virtual void brake() = 0;
+
+    void spin(int pwm)
+    {
+        if (invert_)
+            pwm *= -1;
+
+        if (pwm > 100)
+            forward(pwm);
+        else if (pwm < -100)
+            reverse(pwm);
+        else
+            brake();
+    }
+};
+
+#endif
